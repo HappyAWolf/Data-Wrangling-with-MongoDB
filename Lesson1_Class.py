893b3ce7d89765a596d753f80cdf4a4fe34ba628
Zunlei Xiao <HappyAWolf@users.noreply.github.com>
2015-08-14 09:44:52 -0400

    Create Lesson1_Class.py

---
+++ Lesson1_Class.py
@@ -0,0 +1,46 @@
+# Parsing CSV Files
+
+# Your task is to read the input DATAFILE line by line, and for the first 10 lines (not including the header)
+# split each line on "," and then for each line, create a dictionary
+# where the key is the header title of the field, and the value is the value of that field in the row.
+# The function parse_file should return a list of dictionaries,
+# each data line in the file being a single list entry.
+# Field names and values should not contain extra whitespace, like spaces or newline characters.
+# You can use the Python string method strip() to remove the extra whitespace.
+# You have to parse only the first 10 data lines in this exercise,
+# so the returned list should have 10 entries!
+import os
+
+DATADIR = ""
+DATAFILE = "beatles-diskography.csv"
+
+
+def parse_file(datafile):
+    data = []
+    firstline = True
+    with open(datafile, "r") as f:
+        for line in f:
+            if firstline:
+                firstline = False
+                keys = line.strip().split(',')
+                continue
+            values = line.split(',')
+            record = {}
+            for j in range(len(keys)):
+                record[keys[j]] = values[j].strip()
+            data.append(record)
+    return data
+
+def test():
+    # a simple test of your implemetation
+    datafile = os.path.join(DATADIR, DATAFILE)
+    d = parse_file(datafile)
+    firstline = {'Title': 'Please Please Me', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '22 March 1963', 'US Chart Position': '-', 'RIAA Certification': 'Platinum', 'BPI Certification': 'Gold'}
+    tenthline = {'Title': '', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '10 July 1964', 'US Chart Position': '-', 'RIAA Certification': '', 'BPI Certification': 'Gold'}
+
+    assert d[0] == firstline
+    assert d[9] == tenthline
+
+test()
+
+
