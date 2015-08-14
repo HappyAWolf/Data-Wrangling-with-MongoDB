56b7d3e7f3bf954411b582279409941de1b8d88f
Zunlei Xiao <HappyAWolf@users.noreply.github.com>
2015-08-14 10:26:53 -0400

    Update Lesson2_Problem_Set.py

--- Lesson2_Problem_Set.py
+++ Lesson2_Problem_Set.py
@@ -88,3 +88,177 @@ def test():
 test()
 
 
+# Processing All
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Let's assume that you combined the code from the previous 2 exercises
+# with code from the lesson on how to build requests, and downloaded all the data locally.
+# The files are in a directory "data", named after the carrier and airport:
+# "{}-{}.html".format(carrier, airport), for example "FL-ATL.html".
+# The table with flight info has a table class="dataTDRight".
+# There are couple of helper functions to deal with the data files.
+# Please do not change them for grading purposes.
+# All your changes should be in the 'process_file' function
+# This is example of the datastructure you should return
+# Each item in the list should be a dictionary containing all the relevant data
+# Note - year, month, and the flight data should be integers
+# You should skip the rows that contain the TOTAL data for a year
+# data = [{"courier": "FL",
+#         "airport": "ATL",
+#         "year": 2012,
+#         "month": 12,
+#         "flights": {"domestic": 100,
+#                     "international": 100}
+#         },
+#         {"courier": "..."}
+# ]
+from bs4 import BeautifulSoup
+from zipfile import ZipFile
+import os
+
+datadir = "data"
+
+
+def open_zip(datadir):
+    with ZipFile('{0}.zip'.format(datadir), 'r') as myzip:
+        myzip.extractall()
+
+
+def process_all(datadir):
+    files = os.listdir(datadir)
+    return files
+
+
+def process_file(f):
+    """This is example of the data structure you should return.
+    Each item in the list should be a dictionary containing all the relevant data
+    from each row in each file. Note - year, month, and the flight data should be 
+    integers. You should skip the rows that contain the TOTAL data for a year
+    data = [{"courier": "FL",
+            "airport": "ATL",
+            "year": 2012,
+            "month": 12,
+            "flights": {"domestic": 100,
+                        "international": 100}
+            },
+            {"courier": "..."}
+    ]
+    """
+    data = []
+    info = {}
+    info["courier"], info["airport"] = f[:6].split("-")
+    
+    with open("{}/{}".format(datadir, f), "r") as html:
+
+        soup = BeautifulSoup(html)
+        tr = soup.find_all('tr',{'class':'dataTDRight'})
+        for r in tr:
+            td = r.find_all('td')
+            if td[1].get_text() != "TOTAL":
+                info["year"] = int(td[0].get_text())
+                info["month"] = int(td[1].get_text())
+                info["flights"] = {"domestic": int(td[2].get_text().replace(",","")),
+                                    "international": int(td[3].get_text().replace(",",""))}
+                data.append(info)
+    return data
+
+
+def test():
+    print "Running a simple test..."
+    open_zip(datadir)
+    files = process_all(datadir)
+    data = []
+    for f in files:
+        data += process_file(f)
+        
+    assert len(data) == 399  # Total number of rows
+    for entry in data[:3]:
+        assert type(entry["year"]) == int
+        assert type(entry["month"]) == int
+        assert type(entry["flights"]["domestic"]) == int
+        assert len(entry["airport"]) == 3
+        assert len(entry["courier"]) == 2
+    assert data[-1]["airport"] == "ATL"
+    assert data[-1]["flights"] == {'international': 108289, 'domestic': 701425}
+    
+    print "... success!"
+
+if __name__ == "__main__":
+    test()
+    
+    
+# Patent Database
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This and the following exercise are using US Patent database.
+# The patent.data file is a small excerpt of a much larger datafile
+# that is available for download from US Patent website. They are pretty large ( >100 MB each).
+# The data itself is in XML, however there is a problem with how it's formatted.
+# Please run this script and observe the error. Then find the line that is causing the error.
+# You can do that by just looking at the datafile in the web UI, or programmatically.
+# For quiz purposes it does not matter, but as an exercise we suggest that you try to do it programmatically.
+# The original file is ~600MB large, you might not be able to open it in a text editor.
+# NOTE: You do not need to correct the error - for now, just find where the error is occurring.
+
+import xml.etree.ElementTree as ET
+
+PATENTS = 'patent.data'
+
+def get_root(fname):
+
+    tree = ET.parse(fname)
+    return tree.getroot()
+
+get_root(PATENTS)
+
+
+# Processing Patents
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# So, the problem is that the gigantic file is actually not a valid XML, because
+# it has several root elements, and XML declarations.
+# It is, a matter of fact, a collection of a lot of concatenated XML documents.
+# So, one solution would be to split the file into separate documents,
+# so that you can process the resulting files as valid XML documents.
+
+import xml.etree.ElementTree as ET
+PATENTS = 'patent.data'
+
+def get_root(fname):
+    tree = ET.parse(fname)
+    return tree.getroot()
+
+
+def split_file(filename):
+    # we want you to split the input file into separate files
+    # each containing a single patent.
+    # As a hint - each patent declaration starts with the same line that was causing the error
+    # The new files should be saved with filename in the following format:
+    # "{}-{}".format(filename, n) where n is a counter, starting from 0.
+    with open(filename, 'r') as f:
+        data, n = '', 0
+        for line in f:
+            data += line
+            if line.startswith('</us-patent-grant>'):
+                with open("{}-{}".format(filename, n), 'w') as writer:
+                    writer.write(data)
+                n += 1
+                data = ''
+
+def test():
+    split_file(PATENTS)
+    for n in range(4):
+        try:
+            fname = "{}-{}".format(PATENTS, n)
+            f = open(fname, "r")
+            if not f.readline().startswith("<?xml"):
+                print "You have not split the file {} in the correct boundary!".format(fname)
+            f.close()
+        except:
+            print "Could not find file {}. Check if the filename is correct!".format(fname)
+
+test()
+
