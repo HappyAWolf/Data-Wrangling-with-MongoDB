05889a5867ca9538e83973eed58b1705e382513c
Zunlei Xiao <HappyAWolf@users.noreply.github.com>
2015-08-14 09:46:02 -0400

    Update Lesson1_Class.py

--- Lesson1_Class.py
+++ Lesson1_Class.py
@@ -44,3 +44,74 @@ def test():
 test()
 
 
+# Reading Excel Files
+
+#!/usr/bin/env python
+"""
+Your task is as follows:
+- read the provided Excel file
+- find and return the min, max and average values for the COAST region
+- find and return the time value for the min and max entries
+- the time values should be returned as Python tuples
+
+Please see the test function for the expected return format
+"""
+
+import xlrd
+from zipfile import ZipFile
+datafile = "2013_ERCOT_Hourly_Load_Data.xls"
+
+
+def open_zip(datafile):
+    with ZipFile('{0}.zip'.format(datafile), 'r') as myzip:
+        myzip.extractall()
+
+
+def parse_file(datafile):
+    workbook = xlrd.open_workbook(datafile)
+    sheet = workbook.sheet_by_index(0)
+
+    ### example on how you can get the data
+    sheet_data = [[sheet.cell_value(r, col) for col in range(sheet.ncols)] for r in range(sheet.nrows)]
+    print sheet.ncols, sheet.nrows
+    
+    data = {
+            'maxtime': (0, 0, 0, 0, 0, 0),
+            'maxvalue': 0,
+            'mintime': (0, 0, 0, 0, 0, 0),
+            'minvalue': 0,
+            'avgcoast': 0
+    }
+    
+    column = []
+    max_id = 0
+    min_id = 0
+    data['minvalue'] = sheet_data[1][1]
+    for j in range(1, sheet.nrows):
+        if sheet_data[j][1] >= data['maxvalue']:
+            data['maxvalue'] = sheet_data[j][1]
+            max_id = j
+        if sheet_data[j][1] <= data['minvalue']:
+            data['minvalue'] = sheet_data[j][1]
+            min_id = j
+        column.append(sheet_data[j][1])
+    data['avgcoast'] = sum(column) / len(column)
+    data['maxtime'] = xlrd.xldate_as_tuple(sheet_data[max_id][0], 0)
+    data['mintime'] = xlrd.xldate_as_tuple(sheet_data[min_id][0], 0)
+    
+    return data
+
+
+def test():
+    open_zip(datafile)
+    data = parse_file(datafile)
+
+    assert data['maxtime'] == (2013, 8, 13, 17, 0, 0)
+    assert round(data['maxvalue'], 10) == round(18779.02551, 10)
+
+test()
+
+
+
+
+
